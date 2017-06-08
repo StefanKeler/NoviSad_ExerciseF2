1adf86f9898141a820b374f5c344299749f3ad88
somename <someemail>
2017-06-08 19:52:59 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -9,4 +9,10 @@ public class TestPlanetExplorer {
 		PlanetExplorer p = new PlanetExplorer(3, 3, "");
 		assertNotNull(p);
 	}
+	
+	@Test
+	public void test_dimenzijeGrida(){
+		PlanetExplorer p = new PlanetExplorer(3, 3, "");
+		assertArrayEquals("3x3", p.getGridSize());
+	}
 }
